ef7bd2dbd3bf05edb383429708f083d4b738abd9
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-08-09 22:39:35 +0530

    Add AccountPage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import AccountPage from "./components/AccountPage/AccountPage";
 
 function App() {
 	return (
-		<div>
-			Reddit Clone
+		<div className="center">
+			<AccountPage/>
 		</div>
 	);
 }
